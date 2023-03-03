d33d863c2401aaa95821c11dad8e1a2cf109cec9
Boubaker <boubaker.kakkas@outlook.fr>
2023-03-03 11:57:28 +0100

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //const yearDiv = document.querySelector('#year');
 
-  fetch('whatyearisit-backend-orcin.vercel.app')
-  .then(response => response.text())
-  .then(year => {
-    // Récupérer l'élément avec l'ID "year"
-    const yearElement = document.querySelector('#year');
+  fetch('whatyearisit-backend-orcin.vercel.app/year')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.year)
     
-    // Modifier le contenu de l'élément avec l'année récupérée
-    yearElement.textContent = year;
+    // Récupérer l'élément avec l'ID "year"
+    //const yearElement = document.querySelector('#year').textContent
   })
-  .catch(error => console.error('Erreur lors de la récupération de l\'année :', error));
+  //.catch(error => console.error('Erreur lors de la récupération de l\'année :', error));
